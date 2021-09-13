477e0799c6072f40539a1b91d14062ee0459518c
Callum Morris <callum.morris@outlook.co.nz>
2021-09-13 17:46:37 +1200

    js build navbar for each page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,71 @@
 window.onload = function () {
 
+
+    function loadNavbar() {
+
+        const navbar = document.querySelector('nav')
+    
+        const navbarContent = `<div>
+        <ul class="nav-links">
+            <li><a href='/index.html'>Home</a></li>
+            
+            ${ /* TECHNICAL BLOG */'' }
+
+            <li><a href="#">technical blog</a>
+                <ul class="nav-dropdown-content">
+                    
+                    <li class="nav-dropdown-option">
+                        <a class="nav-dropdown-item"
+                            href="/blog/class-id.html">Classes &amp;
+                            ID\'s</a>
+                    </li>
+
+                    
+                </ul>
+            </li>
+            ${ /* CULTURAL BLOG */'' }
+      
+            <li><a href="#">cultural blog</a>
+                <ul class="nav-dropdown-content">
+                    
+                
+                    <li class="nav-dropdown-option">
+                        <a class="nav-dropdown-item"
+                            href="/blog/identity-values.html">identity
+                            &amp; values</a>
+                    </li>
+                    <li class="nav-dropdown-option">
+                        <a class="nav-dropdown-item"
+                            href="/blog/learning-plan.html">learning
+                            plan</a>
+                    </li>
+                    <li class="nav-dropdown-option">
+                        <a class="nav-dropdown-item"
+                            href="/blog/emotional-intelligence.html">emotional
+                            intelligence</a>
+                    </li>
+                   
+                </ul>
+            </li>
+    
+        </ul>
+        <a href="https://github.com/CalMorris">
+            <img class="social-media-link"
+                src="/blog/img/github-logo.svg"
+                alt="github-link">
+        </a>
+    </div>
+    
+    <div class="hamburger-wrapper">
+        <div class="hamburger-menu"></div>
+    </div>`
+    
+    navbar.innerHTML = navbarContent;
+    }
+    
+    loadNavbar()
+
+function hamburgerDropdown () {
     const links = document.querySelector('.nav-links');
     const hamburgerMenu = document.querySelector('.hamburger-wrapper');
 
@@ -10,4 +76,7 @@ window.onload = function () {
             links.style.display = 'none'
         };
     });
+}
+
+hamburgerDropdown();
 }
